88ec796e3480fcc668b5da9e214d60c8bcf33a90
ponce <contact@gamesfrommars.fr>
2016-05-19 17:56:05 +0200

    Create an opengl window

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Shader Capture
 
 A simple tool to turn a shader into a video.
+
+```bash
+
+$ shader-capture -h
+
+Shader Capture
+
+usage: shader-capture [-width 1920] [-height 1080] [-fps 60] [-duration 1] [-vs vertex-shader.glsl] [-fs fragment-shader.glsl] [-o output.y4m] [-h]
+
+```
--- source/app.d
+++ source/app.d
@@ -1,8 +1,15 @@
 import std.stdio;
 import std.conv;
 import std.string;
+import std.typecons;
 
 import y4md;
+
+import gfm.sdl2,
+       gfm.opengl,
+       gfm.logger,
+       gfm.math;
+
 
 
 
@@ -24,7 +31,7 @@ void main(string[]args)
         string vertexShaderFile = "vertex-shader.glsl";
         string fragmentShaderFile = "fragment-shader.glsl";
         string outputFile = "output.y4m";
-        double durationInSecs = 10;
+        double durationInSecs = 1;
         bool help = false;
 
         for(int i = 1; i < args.length; ++i)
@@ -81,13 +88,30 @@ void main(string[]args)
 
         auto y4mOutput = new Y4MWriter(outputFile, width, height, Rational(fps, 1)); 
         ubyte[] frameBytes = new ubyte[y4mOutput.frameSize()];
-        for (int i = 0; i < numFrames; ++i)
+
+
+        auto window = new CaptureWindow(width, height);
+        scope(exit) window.destroy();
+
+        int iFrame = 0;
+
+        for (; iFrame < numFrames; ++iFrame)
         {
+            window.processEvents();
+
+            if (window.wasQuitRequested())
+                break;
+
             // write something in frameData...
+
+            double time = iFrame / fps;
+
+            window.displayFrame(time);
 
             y4mOutput.writeFrame(frameBytes[]);
         }
 
+        writefln("Written %s frames to %s.", iFrame, outputFile); 
     }
     catch(Exception e)
     {
@@ -95,4 +119,87 @@ void main(string[]args)
         writefln("error: %s", e.msg);
         usage();
     }
+}
+
+
+class CaptureWindow
+{
+    this(int width, int height)
+    {
+        // create a coloured console logger
+        _log = new ConsoleLogger();
+
+        // load dynamic libraries
+        _sdl2 = new SDL2(_log, SharedLibVersion(2, 0, 0));
+        _gl = new OpenGL(_log);
+
+        // You have to initialize each SDL subsystem you want by hand
+        _sdl2.subSystemInit(SDL_INIT_VIDEO);
+        _sdl2.subSystemInit(SDL_INIT_EVENTS);
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+        // create an OpenGL-enabled SDL window
+        _window = new SDL2Window(_sdl2,
+                                 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                 720, 576,
+                                 SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+        // reload OpenGL now that a context exists
+        _gl.reload();
+
+        _texture = new GLTexture2D(_gl);
+        _texture.setMinFilter(GL_LINEAR_MIPMAP_LINEAR);
+        _texture.setMagFilter(GL_LINEAR);
+        _texture.setImage(0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, null); 
+        _texture.generateMipmap();
+    }
+
+    ~this()
+    {
+        _texture.destroy();
+        _window.destroy();
+        _gl.destroy();
+        _sdl2.destroy();
+        _log.destroy();
+    }
+
+    void processEvents()
+    {
+        _sdl2.processEvents();
+    }
+
+    bool wasQuitRequested()
+    {
+        return _sdl2.wasQuitRequested();
+    }
+
+    void displayFrame(double time)
+    {
+        glClearColor(0.5f, 0.5f, 0.5f, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        recomputeTextureContent(time);
+        drawTextureContent();
+        _window.swapBuffers();
+    }
+
+    void recomputeTextureContent(double time)
+    {
+        // TODO draw shader
+    }
+
+    void drawTextureContent()
+    {
+        // TODO draw shader
+    }
+
+    ConsoleLogger _log;
+    SDL2 _sdl2;
+    OpenGL _gl;
+    SDL2Window _window;
+
+    GLTexture2D _texture;
 }
